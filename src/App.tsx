b9cebdeca6bd43a4c6bea35cb72de13cafe5fd43
Sarah Turmel <sarah.turmel@maine.edu>
2024-09-16 21:15:34 -0400

    Final changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,61 @@ import { Button } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { Col } from "react-bootstrap";
 import { Row } from "react-bootstrap";
+import pic from "./1713575453058.png"; // with import
 
 function App(): React.JSX.Element {
     return (
         <Container>
             <Row>
-                <Col>
-                    <div>width: 50%; height: 50%; backgroundColor: red;</div>
+                <div
+                    style={{
+                        backgroundColor: "green",
+                    }}
+                >
                     <h1>This is my header :)</h1>
-                    <p>
-                        Edit <code>src/App.tsx</code> and save. This page will
-                        automatically reload. Hello World! - Sarah Turmel
-                    </p>
-                    <img
-                        src="Onedrive/Sarah - University of Maine System/Slideshow/1713575453058.jpg"
-                        alt="A picture of one of my favorite video game characters"
-                    />
-                </Col>
+                </div>
                 <Col>
-                    <div>width: 50%; height: 50%; backgroundColor: red;</div>
-                    <ol>
-                        <li>Favorite game: Genshin Impact</li>
-                        <li>Second favorite game: Zenless Zone Zero</li>
-                        <li>Third favorite game: The Sims</li>
-                    </ol>
-                    <Button
-                        onClick={() => {
-                            console.log("Hello World");
+                    <div
+                        style={{
+                            width: "50%",
+                            height: "100%",
+                            backgroundColor: "red",
                         }}
                     >
-                        Log Hello World
-                    </Button>
+                        <p>
+                            Edit <code>src/App.tsx</code> and save. This page
+                            will automatically reload. Hello World! - Sarah
+                            Turmel
+                        </p>
+                        <img
+                            src={pic}
+                            alt="A picture of one of my favorite video game characters"
+                            width="100%"
+                            height="70%"
+                        />
+                    </div>
+                </Col>
+                <Col>
+                    <div
+                        style={{
+                            width: "50%",
+                            height: "100%",
+                            backgroundColor: "red",
+                        }}
+                    >
+                        <ol>
+                            <li>Favorite game: Genshin Impact</li>
+                            <li>Second favorite game: Zenless Zone Zero</li>
+                            <li>Third favorite game: The Sims</li>
+                        </ol>
+                        <Button
+                            onClick={() => {
+                                console.log("Hello World");
+                            }}
+                        >
+                            Log Hello World
+                        </Button>
+                    </div>
                 </Col>
             </Row>
         </Container>
