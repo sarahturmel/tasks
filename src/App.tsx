e24f23343b51ab001984ec88d107fa849cf0d5b0
Sarah Turmel <sarah.turmel@maine.edu>
2024-09-17 10:48:22 -0400

    Fixed header issue and missing COS420

--- src/App.tsx
+++ src/App.tsx
@@ -8,60 +8,59 @@ import pic from "./1713575453058.png"; // with import
 
 function App(): React.JSX.Element {
     return (
-        <Container>
-            <Row>
-                <div
-                    style={{
-                        backgroundColor: "green",
-                    }}
-                >
-                    <h1>This is my header :)</h1>
-                </div>
-                <Col>
-                    <div
-                        style={{
-                            width: "50%",
-                            height: "100%",
-                            backgroundColor: "red",
-                        }}
-                    >
-                        <p>
-                            Edit <code>src/App.tsx</code> and save. This page
-                            will automatically reload. Hello World! - Sarah
-                            Turmel
-                        </p>
-                        <img
-                            src={pic}
-                            alt="A picture of one of my favorite video game characters"
-                            width="100%"
-                            height="70%"
-                        />
-                    </div>
-                </Col>
-                <Col>
-                    <div
-                        style={{
-                            width: "50%",
-                            height: "100%",
-                            backgroundColor: "red",
-                        }}
-                    >
-                        <ol>
-                            <li>Favorite game: Genshin Impact</li>
-                            <li>Second favorite game: Zenless Zone Zero</li>
-                            <li>Third favorite game: The Sims</li>
-                        </ol>
-                        <Button
-                            onClick={() => {
-                                console.log("Hello World");
+        <div className="App">
+            <header className="App-header" role="banner">
+                <h1>This is my header: UM COS420 Interactive Textbook Parts</h1>
+            </header>
+
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50%",
+                                height: "100%",
+                                backgroundColor: "red",
                             }}
                         >
-                            Log Hello World
-                        </Button>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
+                            <p>
+                                Edit <code>src/App.tsx</code> and save. This
+                                page will automatically reload. Hello World! -
+                                Sarah Turmel
+                            </p>
+                            <img
+                                src={pic}
+                                alt="A picture of one of my favorite video game characters"
+                                width="100%"
+                                height="70%"
+                            />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                width: "50%",
+                                height: "100%",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            <ol>
+                                <li>Favorite game: Genshin Impact</li>
+                                <li>Second favorite game: Zenless Zone Zero</li>
+                                <li>Third favorite game: The Sims</li>
+                            </ol>
+                            <Button
+                                onClick={() => {
+                                    console.log("Hello World!");
+                                }}
+                            >
+                                Log Hello World
+                            </Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
