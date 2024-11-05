4cd304bf6c8ae2226e9d338c592ba7814de153ea
Sarah Turmel <sarah.turmel@maine.edu>
2024-11-04 20:58:09 -0500

    completed assignment

--- src/App.tsx
+++ src/App.tsx
@@ -12,10 +12,13 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header" role="banner">
-                <h1>This is my header: UM COS420 Interactive Textbook Parts</h1>
+                <h1>
+                    Hello World! This is my header: UM COS420 Interactive
+                    Textbook Parts
+                </h1>
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            {<DoubleHalf></DoubleHalf>}
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
@@ -28,54 +31,6 @@ function App(): React.JSX.Element {
             <TwoDice></TwoDice>
             <hr />
             <ChangeType></ChangeType>
-
-            <Container>
-                <Row>
-                    <Col>
-                        <div
-                            style={{
-                                width: "50%",
-                                height: "100%",
-                                backgroundColor: "red",
-                            }}
-                        >
-                            <p>
-                                Edit <code>src/App.tsx</code> and save. This
-                                page will automatically reload. Hello World! -
-                                Sarah Turmel
-                            </p>
-                            <img
-                                src={pic}
-                                alt="A picture of one of my favorite video game characters"
-                                width="100%"
-                                height="70%"
-                            />
-                        </div>
-                    </Col>
-                    <Col>
-                        <div
-                            style={{
-                                width: "50%",
-                                height: "100%",
-                                backgroundColor: "red",
-                            }}
-                        >
-                            <ol>
-                                <li>Favorite game: Genshin Impact</li>
-                                <li>Second favorite game: Zenless Zone Zero</li>
-                                <li>Third favorite game: The Sims</li>
-                            </ol>
-                            <Button
-                                onClick={() => {
-                                    console.log("Hello World!");
-                                }}
-                            >
-                                Log Hello World
-                            </Button>
-                        </div>
-                    </Col>
-                </Row>
-            </Container>
         </div>
     );
 }
--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,8 +4,15 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+interface ColorIndexState {
+    setColorIndex: (newColorIndex: number) => void;
+    colorIndex: number;
+}
+
+function ChangeColor({
+    colorIndex,
+    setColorIndex,
+}: ColorIndexState): React.JSX.Element {
     return (
         <Button
             onClick={() => {
@@ -17,14 +24,14 @@ function ChangeColor(): React.JSX.Element {
     );
 }
 
-function ColorPreview(): React.JSX.Element {
+function ColorPreview({ colorIndex }: ColorIndexState): React.JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[colorIndex],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px",
@@ -34,13 +41,20 @@ function ColorPreview(): React.JSX.Element {
 }
 
 export function ColoredBox(): React.JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <div>
             <h3>Colored Box</h3>
-            <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
+            <span>The current color is: {COLORS[colorIndex]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    setColorIndex={setColorIndex}
+                    colorIndex={colorIndex}
+                ></ChangeColor>
+                <ColorPreview
+                    setColorIndex={setColorIndex}
+                    colorIndex={colorIndex}
+                ></ColorPreview>
             </div>
         </div>
     );
